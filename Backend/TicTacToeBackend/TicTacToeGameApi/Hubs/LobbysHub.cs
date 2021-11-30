b7e9089797bfd7e05820cb4705da61affe25b7d8
Gosha94 <gerizch@rambler.ru>
2021-11-30 23:14:03 +0300

    rename classes

--- Backend/TicTacToeBackend/TicTacToeGameApi/Hubs/LobbysHub.cs
+++ Backend/TicTacToeBackend/TicTacToeGameApi/Hubs/LobbysHub.cs
@@ -13,10 +13,27 @@ namespace TicTacToeGameApi.Hubs
         public async Task SendPrivateMessageToUser(string user, string message)
             => await Clients.User(user).SendAsync("ReceiveMessage", message);
 
-        public async Task SendMessageToRoom(string roomName, string message)
-            => await Clients.Group(roomName).SendAsync("ReceiveMessage", message);
+        public async Task SendMessageToLobby(string lobbyName, string message)
+            => await Clients.Group(lobbyName).SendAsync("ReceiveMessage", message);
 
-        public async Task JoinToRoom
+
+        // Groups
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
+        }
+
+
+        //public async Task JoinToRoom
 
         //public async Task SendMessageToRoom(string roomName, string message)
         //    => await this.Groups
