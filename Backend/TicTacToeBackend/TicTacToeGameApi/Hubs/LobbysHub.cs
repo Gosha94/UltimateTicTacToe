79f31085d521bccd327623590dcaf3ccb658c66f
Gosha94 <gerizch@rambler.ru>
2021-11-30 20:48:48 +0300

    Create SignalR Api

---
+++ Backend/TicTacToeBackend/TicTacToeGameApi/Hubs/LobbysHub.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace TicTacToeGameApi.Hubs
+{
+    public class LobbysHub : Hub
+    {
+        // Dummies
+        
+        public async Task SendBroadcastMessage(string user, string message)
+            => await Clients.All.SendAsync("ReceiveMessage", user, message);
+
+        public async Task SendPrivateMessageToUser(string user, string message)
+            => await Clients.User(user).SendAsync("ReceiveMessage", message);
+
+        public async Task SendMessageToRoom(string roomName, string message)
+            => await Clients.Group(roomName).SendAsync("ReceiveMessage", message);
+
+        public async Task JoinToRoom
+
+        //public async Task SendMessageToRoom(string roomName, string message)
+        //    => await this.Groups
+
+    }
+}
---
+++ Backend/TicTacToeBackend/TicTacToeGameApi/Services/IGameRoomService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TicTacToeGameApi.Services
+{
+    interface IGameRoomService
+    {
+
+        Task<Guid> CreateGameRoom(string connectionId);
+
+        Task<Guid> GetGameRoomForConnectionId(string connectionId);
+
+    }
+}
